3ba0d05790528fdcf141b55e7b6735f52d1bc4b6
Van Thong <vanthongvictory2002@gmail.com>
2023-04-30 13:42:53 +0700

    update booking+pay+admin

--- CODE/app/controller.py
+++ CODE/app/controller.py
@@ -1,6 +1,11 @@
 from ProjectHotel.settings import execute_query
 
 class Phong_controller:
+    def get_room_item(maphong):
+        sql = "select * from phong ph,loaiphong lp where lp.MALOAIPHONG = ph.LOAIP and ph.MAPHONG = '{0}'".format(maphong)
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result[0]
+     
     def get_typeroom():
         sql = "select * from LoaiPhong"
         result = execute_query('QLKhachSan', 'a' ,sql)
@@ -25,6 +30,7 @@ class Phong_controller:
         sql = "select * from phong ph join loaiphong lp on lp.MALOAIPHONG = ph.LOAIP where lp.MALOAIPHONG = '{0}'".format(malp)
         result = execute_query('QLKhachSan', 'a' ,sql)
         return result
+    
     def get_nametype(malp):
         sql = "select TENLOAIPHONG from LoaiPhong lp where lp.MALOAIPHONG = '{0}'".format(malp)
         result = execute_query('QLKhachSan', 'a' ,sql)
@@ -55,3 +61,79 @@ class signup_controller:
         sql = "INSERT INTO TaiKhoan (Username, Password, Type) VALUES ('{0}','{1}', 2)".format(username, password)
         result ,er= execute_query('QLKhachSan', 'a' ,sql)
         return result ,er
+    
+class booking_controller:
+    def get_idcus(username):
+        sql = "SELECT CMND FROM khachhang where USERNAME = '{0}'".format(username)
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result[0][0]
+    
+    def get_countpdp():
+        sql = "SELECT COUNT(*) FROM PhieuDatPhong"
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result[0][0]
+    
+    def get_infocus(makh):
+        sql = "SELECT * FROM khachhang where CMND = '{0}'".format(makh)
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result[0]
+    
+    def get_countpay():
+        sql = "SELECT COUNT(*) FROM thongtinthanhtoan"
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result[0][0]
+    
+    def save_infopay(matt, type):
+        if type == 'pay':
+            type_new = 1
+
+        if type == 'momo':
+            type_new = 2
+
+        if type == 'zalo':
+            type_new = 3
+
+        sql = "INSERT INTO thongtinthanhtoan VALUES ('{0}','{1}')".format(matt, type_new)
+        result ,er= execute_query('QLKhachSan', 'a' ,sql)
+        return result ,er
+    
+    def save_pay(mapay,sothe, tenthe, ngayhethan, cvc, matt):
+        sql = "INSERT INTO thenganhang VALUES ('{0}','{1}','{2}', '{3}', '{4}','{5}')".format(mapay, sothe, tenthe, ngayhethan, cvc, matt)
+        result ,er= execute_query('QLKhachSan', 'a' ,sql)
+        return result ,er
+    
+    def get_countthe():
+        sql = "SELECT COUNT(*) FROM thenganhang"
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result[0][0]
+    
+    def save_momo(mamomo, sdt, matt):
+        sql = "INSERT INTO momo VALUES ('{0}','{1}','{2}')".format(mamomo,sdt, matt)
+        result ,er= execute_query('QLKhachSan', 'a' ,sql)
+        return result ,er
+    
+    def get_countmomo():
+        sql = "SELECT COUNT(*) FROM momo"
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result[0][0]
+    
+    def save_zalo(mazalo, sdt, matt):
+        sql = "INSERT INTO zalo VALUES ('{0}','{1}','{2}')".format(mazalo,sdt, matt)
+        result ,er= execute_query('QLKhachSan', 'a' ,sql)
+        return result ,er
+    
+    def get_countzalo():
+        sql = "SELECT COUNT(*) FROM Zalo"
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result[0][0]
+    
+    def save_booking(mapdp, ngayden, sodem, slng, stc, maphong, makh, matt):
+        sql = "INSERT INTO phieudatphong (MAPDP, NGAYDEN, SODEMLUTRU,YEUCAUDACBIET, SOLUONGNGUOI, SOTIENDATCOC,CHECKDC, MAPHONG, MAKH, MATT) VALUES ('{0}', '{1}', '{2}', '', '{3}', '{4}', '', '{5}','{6}','{7}')".format(mapdp, ngayden, sodem, slng, stc, maphong, makh, matt)
+        result, er = execute_query('QLKhachSan', 'a' ,sql)
+        return result, er
+    
+class admin_controller():
+    def get_room():
+        sql ="SELECT * FROM phong ph, loaiphong lp where lp.MALOAIPHONG = ph.loaip"
+        result = execute_query('QLKhachSan', 'a' ,sql)
+        return result
